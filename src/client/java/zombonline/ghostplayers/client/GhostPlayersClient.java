4c5c5ff18231fb4d5ac63c6dd5959bb2453a098e
zombonline <megazombie130@gmail.com>
2026-09-17 14:31:42 +0100

    fabric template

---
+++ src/client/java/zombonline/ghostplayers/client/GhostPlayersClient.java
@@ -0,0 +1,10 @@
+package zombonline.ghostplayers.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GhostPlayersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
